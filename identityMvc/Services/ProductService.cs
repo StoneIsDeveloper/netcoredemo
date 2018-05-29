51d1df66238b9043cea4435d0205f16a70cbfbeb
StoneIsDeveloper <m18146611430@163.com>
2018-05-29 16:23:07 +0800

    INTI

---
+++ identityMvc/Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using identityMvc.Data;
+using identityMvc.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace identityMvc.Services
+{
+    public interface IProductService
+    {
+        Task<int> AddProduct(Product entity);
+        bool DeltProduct(string id);
+        List<Product> GetProduct(Expression<Func<Product, bool>> fun);
+    }
+
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Task<int> AddProduct(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeltProduct(string id)
+        {
+            var product = _context.Products.SingleOrDefault(m => m.Id == id);
+            if(product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChangesAsync();
+                return true ;
+            }
+            else
+            {
+                return false;
+            }
+            //throw new NotImplementedException();
+        }
+
+        public List<Product> GetProduct(Expression<Func<Product, bool>> fun)
+        {
+            return _context.Products.ToList();
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ identityMvc/obj/Debug/netcoreapp2.0/4ii0voyu.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'__EFMigrationsHistory') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Products] (
+    [Id] nvarchar(450) NOT NULL,
+    [Description] nvarchar(max) NULL,
+    [Name] nvarchar(max) NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20180528013710_AddProduct', N'2.0.3-rtm-10026');
+
+GO
+
